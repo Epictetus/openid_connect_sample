17ff2e88a34696c7a6a2f5d91be65ef438b721df
nov matake <nov@matake.jp>
2011-08-12 21:45:58 +0900

    logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 OpenidConnectSample::Application.routes.draw do
   resource :facebook,  only: :show
+  resource :session,   only: :destroy
   resource :dashboard, only: :show
   resources :clients,  only: [:show, :new, :create, :destroy]
 
