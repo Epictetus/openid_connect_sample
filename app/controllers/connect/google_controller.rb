4b310d9b0e00304cd4cdd0cd5b75b1a58dd76648
nov matake <nov@matake.jp>
2011-08-14 22:35:19 +0900

    deny access response handling

--- app/controllers/connect/google_controller.rb
+++ app/controllers/connect/google_controller.rb
@@ -2,6 +2,7 @@ class Connect::GoogleController < ApplicationController
   before_filter :require_anonymous_access
 
   def show
+    raise AuthenticationRequired.new('Authorization Code Required') unless params[:code]
     authenticate Connect::Google.authenticate(params[:code])
     redirect_to dashboard_url
   end
